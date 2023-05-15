761f899fb1a7b4ad03bb65b7ea878567d3a2221f
PeturIvanov <116626395+PeturIvanov@users.noreply.github.com>
2023-05-15 21:51:29 +0300

    Update 05_longest_intersection.py

--- python_advanced/advanced/Tuples and Sets - Exercise/05_longest_intersection.py
+++ python_advanced/advanced/Tuples and Sets - Exercise/05_longest_intersection.py
@@ -1,6 +1,6 @@
 number_of_lines = int(input())
 
-longest_intersection = 0
+longest_intersection = set()
 
 for _ in range(number_of_lines):
     first_range, second_range = input().split("-")
@@ -11,13 +11,11 @@ for _ in range(number_of_lines):
     first_intersection = set([i for i in range(first_start, first_end + 1)])
     second_intersection = set([i for i in range(second_start, second_end + 1)])
 
-    current_intersection = list(first_intersection.intersection(second_intersection))
+    current_intersection = first_intersection.intersection(second_intersection)
 
-    if longest_intersection == 0:
-        longest_intersection = current_intersection
-    else:
-        if len(longest_intersection) < len(current_intersection):
-            longest_intersection = current_intersection
+    if len(current_intersection) > len(longest_intersection):
+        longest_intersection = list(current_intersection)
+
 
 print(f"Longest intersection is {longest_intersection} with length {len(longest_intersection)}")
 
