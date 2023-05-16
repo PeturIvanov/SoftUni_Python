e8624937bfac23df5c341d3044f9364bef774ca1
PeturIvanov <116626395+PeturIvanov@users.noreply.github.com>
2023-05-16 13:15:52 +0300

    Update 02_expression_evaluator.py

--- python_advanced/advanced/Stacks, Queues, Tuples and Sets - Exercise/02_expression_evaluator.py
+++ python_advanced/advanced/Stacks, Queues, Tuples and Sets - Exercise/02_expression_evaluator.py
@@ -1,9 +1,11 @@
 from collections import deque
+from functools import reduce
 
 string = deque(input().split())
 
 numbers = deque()
 
+# Solution using reduce module
 while string:
     char = string.popleft()
 
@@ -11,25 +13,56 @@ while string:
         numbers.append(int(char))
 
     else:
-        result = numbers.popleft()
+        if char == "-":
+            result = reduce(lambda a, b: a - b, numbers)
 
-        while numbers:
-            digit = numbers.popleft()
+        elif char == "+":
+            result = sum(numbers)
 
-            if char == "-":
-                result -= digit
+        elif char == "*":
+            result = reduce(lambda a, b: a * b, numbers)
 
-            elif char == "+":
-                result += digit
+        else:
+            result = reduce(lambda a, b: a // b, numbers)
 
-            elif char == "*":
-                result *= digit
-
-            else:
-                result //= digit
-                
         string.appendleft(str(result))
+        numbers.clear()
 
         if len(string) == 1:
             print(*string)
             break
+
+# Second solution
+# string = deque(input().split())
+#
+# numbers = deque()
+#
+# while string:
+#     char = string.popleft()
+#
+#     if char not in "-*/+":
+#         numbers.append(int(char))
+#
+#     else:
+#         result = numbers.popleft()
+#
+#         while numbers:
+#             digit = numbers.popleft()
+#
+#             if char == "-":
+#                 result -= digit
+#
+#             elif char == "+":
+#                 result += digit
+#
+#             elif char == "*":
+#                 result *= digit
+#
+#             else:
+#                 result //= digit
+#
+#         string.appendleft(str(result))
+#
+#         if len(string) == 1:
+#             print(*string)
+#             break
