880699c545fa074b1b4bd97e1959b856deb08cc6
PeturIvanov <116626395+PeturIvanov@users.noreply.github.com>
2023-05-16 09:05:54 +0300

    Create 01_numbers.py
    Solve problem 1

---
+++ python_advanced/advanced/Stacks, Queues, Tuples and Sets - Exercise/01_numbers.py
@@ -0,0 +1,37 @@
+first_sequence = set(int(n) for n in input().split())
+second_sequence = set(int(n) for n in input().split())
+
+number_of_lines = int(input())
+
+for _ in range(number_of_lines):
+    input_data = input().split()
+
+    command = input_data[0]
+
+    if command == "Add":
+        numbers_to_add = set(map(int, input_data[2:]))
+
+        if input_data[1] == "First":
+            first_sequence = first_sequence.union(numbers_to_add)
+
+        else:
+            second_sequence = second_sequence.union(numbers_to_add)
+
+    elif command == "Remove":
+        numbers_to_remove = set(map(int, input_data[2:]))
+
+        if input_data[1] == "First":
+            set(first_sequence.remove(n) for n in numbers_to_remove if n in first_sequence)
+
+        else:
+            set(second_sequence.remove(n) for n in numbers_to_remove if n in second_sequence)
+
+    else:
+        if len(first_sequence) >= len(second_sequence):
+            print(second_sequence.issubset(first_sequence))
+        else:
+            print(first_sequence.issubset(second_sequence))
+
+print(*(sorted(first_sequence)), sep=", ")
+print(*(sorted(second_sequence)), sep=", ")
+
