5e99bcb1fc7edb06d481337f510e6f9578a3a6ad
PeturIvanov <116626395+PeturIvanov@users.noreply.github.com>
2023-05-17 09:09:54 +0300

    Update 06_paint_colors.py

--- python_advanced/advanced/Stacks, Queues, Tuples and Sets - Exercise/06_paint_colors.py
+++ python_advanced/advanced/Stacks, Queues, Tuples and Sets - Exercise/06_paint_colors.py
@@ -1,7 +1,7 @@
 from collections import deque
 
 substrings = deque(input().split())
-all_colors = ["red", "yellow", "blue", "orange", "purple", "green"]
+all_colors = {"red", "yellow", "blue", "orange", "purple", "green"}
 
 secondary_colors = {
     "orange": {"red", "yellow"},
@@ -14,24 +14,19 @@ while substrings:
     first_subst = substrings.popleft()
     second_subst = substrings.pop() if substrings else ""
 
-    first_str = f"{first_subst}{second_subst}"
-    second_str = f"{second_subst}{first_subst}"
-
-    if first_str in all_colors or second_str in all_colors:
-        find_colors.append(first_str if first_str in all_colors else second_str)
+    for color in (first_subst + second_subst, second_subst + first_subst):
+        if color in all_colors:
+            find_colors.append(color)
+            break
 
     else:
-        first_subst = first_subst[:len(first_subst) - 1]
-        second_subst = second_subst[:len(second_subst) - 1]
-        if first_subst:
-            substrings.insert(len(substrings) // 2, first_subst)
-        if second_subst:
-            substrings.insert(len(substrings) // 2, second_subst)
+        for el in (first_subst[:-1], second_subst[:-1]):
+            if el:
+                substrings.insert(len(substrings) // 2, el)
 
-for el in find_colors:
-    if el in secondary_colors:
-        if not secondary_colors[el].issubset(find_colors):
-            find_colors.remove(el)
 
+for el in set(secondary_colors.keys()).intersection(find_colors):
+    if not secondary_colors[el].issubset(find_colors):
+        find_colors.remove(el)
 
 print(find_colors)
