55474d4d68540986a13a9a31ff2535f4e67fc0eb
PeturIvanov <116626395+PeturIvanov@users.noreply.github.com>
2023-05-18 13:40:45 +0300

    Create 06_matrix_shuffling.py
    Solve problem 6

---
+++ python_advanced/advanced/Multidimensional Lists - Exercise 1/06_matrix_shuffling.py
@@ -0,0 +1,37 @@
+def valid_input(indexes, command):
+    if command != "swap" or len(indexes) != 4:
+        return False
+
+    if (indexes[0] >= rows or 0 > indexes[0]) or (indexes[2] >= rows or 0 > indexes[2]):
+        return False
+    elif (indexes[1] >= columns or 0 > indexes[1]) or (indexes[3] >= columns or 0 > indexes[3]):
+        return False
+
+    return True
+
+
+rows, columns = [int(x) for x in input().split()]
+matrix = [input().split() for _ in range(rows)]
+
+while True:
+    command, *data = input().split()
+
+    if command == "END":
+        break
+
+    data = [int(i) for i in data if i.isdigit()]
+
+    if valid_input(data, command):
+        first_el_start_idx, first_el_end_idx = data[:2]
+        second_el_start_idx, second_el_end_idx = data[2:]
+
+        first_el = matrix[first_el_start_idx][first_el_end_idx]
+        second_el = matrix[second_el_start_idx][second_el_end_idx]
+
+        matrix[first_el_start_idx][first_el_end_idx], matrix[second_el_start_idx][second_el_end_idx] = \
+            matrix[second_el_start_idx][second_el_end_idx], matrix[first_el_start_idx][first_el_end_idx]
+
+        for el in matrix:
+            print(*el)
+    else:
+        print("Invalid input!")
