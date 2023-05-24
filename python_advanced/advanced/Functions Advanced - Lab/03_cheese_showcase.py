db3a91746f274f5fc954d0d3af29626e0e1789dc
PeturIvanov <116626395+PeturIvanov@users.noreply.github.com>
2023-05-24 13:58:09 +0300

    Create 03_cheese_showcase.py
    Solve problem 3

---
+++ python_advanced/advanced/Functions Advanced - Lab/03_cheese_showcase.py
@@ -0,0 +1,20 @@
+def sorting_cheeses(**kwargs):
+    result = ""
+    sorted_cheeses = sorted(kwargs.items(), key=lambda x: (-len(x[1]), x[0]))
+    for cheese_name, quantity in sorted_cheeses:
+        result += f"{cheese_name}\n"
+        sorted_values = sorted(quantity, reverse=True)
+        result += '\n'.join(str(x) for x in sorted_values) + "\n"
+
+    return result
+
+
+
+print(
+ sorting_cheeses(
+ Parmesan=[102, 120, 135],
+ Camembert=[100, 100, 105, 500, 430],
+ Mozzarella=[50, 125],
+ )
+)
+
