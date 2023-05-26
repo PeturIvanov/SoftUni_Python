b1f1211b1f74a4c10de9aa77266a0dfb5a93b664
PeturIvanov <116626395+PeturIvanov@users.noreply.github.com>
2023-05-26 21:57:47 +0300

    Update 10_fill_tthe_box.py

--- python_advanced/advanced/Functions Advanced - Exercise/10_fill_tthe_box.py
+++ python_advanced/advanced/Functions Advanced - Exercise/10_fill_tthe_box.py
@@ -5,28 +5,18 @@ def fill_the_box(height, length, width, *data):
     box_space = height * length * width
     data = deque(data)
 
-    while data and box_space > 0:
+    while data[0] != "Finish":
         cubes_amount = data.popleft()
 
-        if cubes_amount == "Finish":
-            break
+        box_space -= cubes_amount
 
-        if box_space - cubes_amount >= 0:
-            box_space -= cubes_amount
+        if box_space < 0:
+            cubes_left = sum([el for el in data if el != "Finish"])
+            return f"No more free space! You have {cubes_left + abs(box_space)} more cubes."
 
-        else:
-            cubes_remain = cubes_amount - box_space
-            box_space -= (cubes_amount - cubes_remain)
-            data.append(cubes_remain)
-
-    if box_space > 0:
-        return f"There is free space in the box. You could put {box_space} more cubes."
-    else:
-        cubes_left = sum([el for el in data if el != "Finish"])
-        return f"No more free space! You have {cubes_left} more cubes."
+    return f"There is free space in the box. You could put {box_space} more cubes."
 
 
-print(fill_the_box(10, 10,
-10, 40, "Finish", 2, 15,
-30))
-
+print(fill_the_box(2, 8,
+2, 2, 1, 7, 3, 1, 5,
+"Finish"))
