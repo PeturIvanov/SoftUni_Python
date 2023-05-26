1ee58dcfa54e4a919d847605b221a8fbfce385d3
PeturIvanov <116626395+PeturIvanov@users.noreply.github.com>
2023-05-26 19:21:48 +0300

    Update 07_grocery.py

--- python_advanced/advanced/Functions Advanced - Exercise/07_grocery.py
+++ python_advanced/advanced/Functions Advanced - Exercise/07_grocery.py
@@ -1,7 +1,7 @@
 def grocery_store(**products):
     sorted_products = sorted(products.items(), key=lambda x: (-x[1], -len(x[0]), x[0]))
-    result = "\n".join([f"{product}: {value}" for product, value in sorted_products])
-    return result
+    return "\n".join([f"{product}: {value}" for product, value in sorted_products])
+
 
 
 print(grocery_store(
