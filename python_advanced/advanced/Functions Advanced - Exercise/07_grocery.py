784dee49a504b34efa9179fa31b9e314722725fc
PeturIvanov <116626395+PeturIvanov@users.noreply.github.com>
2023-05-26 18:57:30 +0300

    Create 07_grocery.py
    Solve problem 7

---
+++ python_advanced/advanced/Functions Advanced - Exercise/07_grocery.py
@@ -0,0 +1,13 @@
+def grocery_store(**products):
+    sorted_products = sorted(products.items(), key=lambda x: (-x[1], -len(x[0]), x[0]))
+    result = "\n".join([f"{product}: {value}" for product, value in sorted_products])
+    return result
+
+
+print(grocery_store(
+ bread=2,
+ pasta=2,
+ eggs=20,
+ carrot=1,
+))
+
