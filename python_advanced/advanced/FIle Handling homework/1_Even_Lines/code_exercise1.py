31388b77a865ebb493125ac6f29106cf9f43a09a
PeturIvanov <116626395+PeturIvanov@users.noreply.github.com>
2023-05-31 14:27:13 +0300

    Create code_exercise1.py
    Solve problem 1

---
+++ python_advanced/advanced/FIle Handling homework/1_Even_Lines/code_exercise1.py
@@ -0,0 +1,18 @@
+import os
+import re
+
+root_path = os.path.dirname(os.path.abspath(__file__))
+
+file_name = "even_lines_.txt"
+
+file_path = os.path.join(root_path, file_name)
+
+pattern = r"[\.\,\?\-\!]"
+with open(file_path, "r") as file:
+    file_content = file.read()
+    file_content = re.sub(pattern, "@", file_content)
+
+for i, line in enumerate(file_content.split("\n")):
+    if i % 2 == 0:
+        print(*[el for el in line.split()[::-1]])
+
