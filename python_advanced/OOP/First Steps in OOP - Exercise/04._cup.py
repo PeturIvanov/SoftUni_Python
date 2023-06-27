feea2181c28ad6e38afa26a2a8a98c194d2c5579
PeturIvanov <116626395+PeturIvanov@users.noreply.github.com>
2023-06-27 11:35:04 +0300

    Update 04._cup.py

--- python_advanced/OOP/First Steps in OOP - Exercise/04._cup.py
+++ python_advanced/OOP/First Steps in OOP - Exercise/04._cup.py
@@ -1,14 +1,15 @@
 class Cup:
-    def __init__(self, size, quantity):
+    def __init__(self, size: int, quantity: int):
         self.size = size
         self.quantity = quantity
 
-    def fill(self, quantity):
+    def fill(self, quantity) -> None:
         if self.quantity + quantity <= self.size:
             self.quantity += quantity
 
-    def status(self):
+    def status(self) -> int:
         free_space = self.size - self.quantity
+        
         return free_space
 
 
