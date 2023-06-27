84b85e27454abdc50e48e7c34dc925def43039fa
PeturIvanov <116626395+PeturIvanov@users.noreply.github.com>
2023-06-27 11:32:46 +0300

    Update 01_shop.py

--- python_advanced/OOP/First Steps in OOP - Exercise/01_shop.py
+++ python_advanced/OOP/First Steps in OOP - Exercise/01_shop.py
@@ -1,9 +1,12 @@
+from typing import List
+
+
 class Shop:
-    def __init__(self, name, items):
+    def __init__(self, name: str, items: List[str]):
         self.name = name
         self.items = items
 
-    def get_items_count(self):
+    def get_items_count(self) -> int:
         return len(self.items)
 
 
