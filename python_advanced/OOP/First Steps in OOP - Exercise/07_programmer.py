4b526f047ad064ae182407a5187a34d5ebdb6d7d
PeturIvanov <116626395+PeturIvanov@users.noreply.github.com>
2023-06-27 11:38:18 +0300

    Update 07_programmer.py

--- python_advanced/OOP/First Steps in OOP - Exercise/07_programmer.py
+++ python_advanced/OOP/First Steps in OOP - Exercise/07_programmer.py
@@ -1,20 +1,22 @@
 class Programmer:
-    def __init__(self, name, language, skills):
+    def __init__(self, name: str, language: str, skills: int):
         self.name = name
         self.language = language
         self.skills = skills
 
-    def watch_course(self, course_name, language, skills_earned):
+    def watch_course(self, course_name: str, language: str, skills_earned: int) -> str:
         if language == self.language:
             self.skills += skills_earned
             return f"{self.name} watched {course_name}"
 
         return f"{self.name} does not know {language}"
 
-    def change_language(self, new_language, skills_needed):
+    def change_language(self, new_language: str, skills_needed: int) -> str:
+
         if self.language != new_language and skills_needed <= self.skills:
             old_language = self.language
             self.language = new_language
+
             return f"{self.name} switched from {old_language} to {new_language}"
 
         elif self.language == new_language and skills_needed <= self.skills:
