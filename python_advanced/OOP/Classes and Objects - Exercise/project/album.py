3d56929bba29a152df50bd3016a8f86763685a56
PeturIvanov <116626395+PeturIvanov@users.noreply.github.com>
2023-06-29 11:13:07 +0300

    07. Spoopify

---
+++ python_advanced/OOP/Classes and Objects - Exercise/project/album.py
@@ -0,0 +1,49 @@
+from project.song import Song
+
+class Album:
+    def __init__(self, name: str, *songs: tuple or str):
+        self.name = name
+        self.published = False
+        self.songs = [s for s in songs]
+
+    def add_song(self, song: Song) -> str:
+        if song.single:
+            return f"Cannot add {song.name}. It's a single"
+
+        if self.published:
+            return "Cannot add songs. Album is published."
+
+        if song in self.songs:
+            return f"Song is already in the album."
+
+        self.songs.append(song)
+
+        return f"Song {song.name} has been added to the album {self.name}."
+
+    def remove_song(self, song_name: str) -> str:
+        if self.published:
+            return "Cannot remove songs. Album is published."
+
+        try:
+            song_instance = next(filter(lambda s: s.name == song_name, self.songs))
+
+            self.songs.remove(song_instance)
+
+            return f"Removed song {song_name} from album {self.name}."
+
+        except StopIteration:
+            return "Song is not in the album."
+
+    def publish(self) -> str:
+        if self.published:
+            return f"Album {self.name} is already published."
+
+        self.published = True
+
+        return f"Album {self.name} has been published."
+
+    def details(self) -> str:
+        info = [f"Album {self.name}"]
+        info.extend([f"== {song.get_info()}" for song in self.songs])
+
+        return "\n".join(info)
---
+++ python_advanced/OOP/Classes and Objects - Exercise/project/band.py
@@ -0,0 +1,189 @@
+from project.album import Album
+from project.song import Song
+
+class Band:
+    def __init__(self, name: str):
+        self.name = name
+        self.albums = []
+
+    def add_album(self, album: Album) -> str:
+        if album in self.albums:
+            return f"Band {self.name} already has {album.name} in their library."
+
+        self.albums.append(album)
+
+        return f"Band {self.name} has added their newest album {album.name}."
+
+    def remove_album(self, album_name: str) -> str:
+        try:
+            album_instance = next(filter(lambda a: a.name == album_name, self.albums))
+
+        except StopIteration:
+            return f"Album {album_name} is not found."
+
+        if album_instance.published:
+            return f"Album has been published. It cannot be removed."
+
+        self.albums.remove(album_instance)
+
+        return f"Album {album_name} has been removed."
+
+    def details(self) -> str:
+        info = [f"Band {self.name}"]
+        info.extend([album.details() for album in self.albums])
+
+        return '\n'.join(info)
+
+
+import unittest
+
+
+class SongTest(unittest.TestCase):
+    def test_song_init(self):
+        song = Song("A", 3.15, False)
+        message = song.get_info()
+        expected = "A - 3.15"
+        self.assertEqual(message, expected)
+
+    def test_album_init(self):
+        album = Album("The Sound of Perseverance")
+        message = album.details().strip()
+        expected = "Album The Sound of Perseverance"
+        self.assertEqual(message, expected)
+
+    def test_add_song_working(self):
+        album = Album("The Sound of Perseverance")
+        song = Song("Scavenger of Human Sorrow", 6.56, False)
+        message = album.add_song(song)
+        expected = "Song Scavenger of Human Sorrow has been added to the album The Sound of Perseverance."
+        self.assertEqual(message, expected)
+
+    def test_add_song_already_added(self):
+        album = Album("The Sound of Perseverance")
+        song = Song("Scavenger of Human Sorrow", 6.56, False)
+        album.add_song(song)
+        message = album.add_song(song)
+        expected = "Song is already in the album."
+        self.assertEqual(message, expected)
+
+    def test_add_song_single(self):
+        album = Album("The Sound of Perseverance")
+        song = Song("Scavenger of Human Sorrow", 6.56, True)
+        message = album.add_song(song)
+        expected = "Cannot add Scavenger of Human Sorrow. It's a single"
+        self.assertEqual(message, expected)
+
+    def test_add_song_published_album(self):
+        album = Album("The Sound of Perseverance")
+        song = Song("Scavenger of Human Sorrow", 6.56, False)
+        album.publish()
+        message = album.add_song(song)
+        expected = "Cannot add songs. Album is published."
+        self.assertEqual(message, expected)
+
+    def test_remove_song_working(self):
+        album = Album("The Sound of Perseverance")
+        song = Song("Scavenger of Human Sorrow", 6.56, False)
+        album.add_song(song)
+        message = album.remove_song("Scavenger of Human Sorrow")
+        expected = "Removed song Scavenger of Human Sorrow from album The Sound of Perseverance."
+        self.assertEqual(message, expected)
+
+    def test_remove_song_not_in_album(self):
+        album = Album("The Sound of Perseverance")
+        song = Song("Scavenger of Human Sorrow", 6.56, False)
+        message = album.remove_song("Scavenger of Human Sorrow")
+        expected = "Song is not in the album."
+        self.assertEqual(message, expected)
+
+    def test_remove_song_album_published(self):
+        album = Album("The Sound of Perseverance")
+        song = Song("Scavenger of Human Sorrow", 6.56, False)
+        album.add_song(song)
+        album.publish()
+        message = album.remove_song("Scavenger of Human Sorrow")
+        expected = "Cannot remove songs. Album is published."
+        self.assertEqual(message, expected)
+
+    def test_publish(self):
+        album = Album("The Sound of Perseverance")
+        message = album.publish()
+        expected = album.published
+        self.assertTrue(expected)
+
+    def test_publish_message(self):
+        album = Album("The Sound of Perseverance")
+        message = album.publish()
+        expected = "Album The Sound of Perseverance has been published."
+        self.assertEqual(message, expected)
+
+    def test_details(self):
+        album = Album("The Sound of Perseverance")
+        song = Song("Scavenger of Human Sorrow", 6.56, False)
+        album.add_song(song)
+        message = album.details().strip()
+        expected = "Album The Sound of Perseverance\n== Scavenger of Human Sorrow - 6.56"
+
+    def test_init(self):
+        band = Band("Death")
+        message = f"{band.name} - {len(band.albums)}"
+        expected = "Death - 0"
+        self.assertEqual(message, expected)
+
+    def test_add_album_working(self):
+        band = Band("Death")
+        album = Album("The Sound of Perseverance")
+        message = band.add_album(album)
+        expected = "Band Death has added their newest album The Sound of Perseverance."
+        self.assertEqual(message, expected)
+
+    def test_add_album_already_added(self):
+        band = Band("Death")
+        album = Album("The Sound of Perseverance")
+        band.add_album(album)
+        message = band.add_album(album)
+        expected = "Band Death already has The Sound of Perseverance in their library."
+        self.assertEqual(message, expected)
+
+    def test_remove_album_working(self):
+        band = Band("Death")
+        album = Album("The Sound of Perseverance")
+        band.add_album(album)
+        message = band.remove_album("The Sound of Perseverance")
+        expected = "Album The Sound of Perseverance has been removed."
+        self.assertEqual(message, expected)
+
+    def test_remove_album_not_found(self):
+        band = Band("Death")
+        album = Album("The Sound of Perseverance")
+        message = band.remove_album("The Sound of Perseverance")
+        expected = "Album The Sound of Perseverance is not found."
+        self.assertEqual(message, expected)
+
+    def test_remove_album_published(self):
+        band = Band("Death")
+        album = Album("The Sound of Perseverance")
+        album.publish()
+        band.add_album(album)
+        message = band.remove_album("The Sound of Perseverance")
+        expected = "Album has been published. It cannot be removed."
+        self.assertEqual(message, expected)
+
+    def test_details(self):
+        band = Band("Death")
+        message = band.details().strip()
+        expected = "Band Death"
+        self.assertEqual(message, expected)
+
+
+
+
+
+
+
+
+
+
+
+
+
