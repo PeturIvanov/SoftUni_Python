86b676135039123efbb9762fceab19bbcc26dd6d
PeturIvanov <116626395+PeturIvanov@users.noreply.github.com>
2023-06-29 09:41:11 +0300

    rename

--- python_advanced/OOP/Classes and Objects - Exercise/to_do_list/section.py
+++ python_advanced/OOP/Classes and Objects - Exercise/to_do_list/section.py
@@ -1,4 +1,4 @@
-from project.task import Task
+from to_do_list.task import Task
 class Section:
     def __init__(self, name: str):
         self.name = name
@@ -37,5 +37,19 @@ class Section:
         section_data.extend([t.details() for t in self.tasks])
 
         return '\n'.join(section_data)
+
+
+task = Task("Make bed", "27/05/2020")
+print(task.change_name("Go to University"))
+print(task.change_due_date("28.05.2020"))
+task.add_comment("Don't forget laptop")
+print(task.edit_comment(0, "Don't forget laptop and notebook"))
+print(task.details())
+section = Section("Daily tasks")
+print(section.add_task(task))
+second_task = Task("Make bed", "27/05/2020")
+section.add_task(second_task)
+print(section.clean_section())
+print(section.view_section())
 
 
