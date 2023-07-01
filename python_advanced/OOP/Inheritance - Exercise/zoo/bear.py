8792700659672a013534d5ba48590fdfdee4f33b
PeturIvanov <116626395+PeturIvanov@users.noreply.github.com>
2023-07-01 10:24:29 +0300

    02. Zoo

---
+++ python_advanced/OOP/Inheritance - Exercise/zoo/bear.py
@@ -0,0 +1,6 @@
+from zoo.mammal import Mammal
+
+
+class Bear(Mammal):
+    def __init__(self, name: str):
+        super().__init__(name)
---
+++ python_advanced/OOP/Inheritance - Exercise/zoo/mammal.py
@@ -0,0 +1,6 @@
+from zoo.animal import Animal
+
+
+class Mammal(Animal):
+    def __init__(self, name: str):
+        super().__init__(name)
