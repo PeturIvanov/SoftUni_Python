7605d3ec4f7f3a7f3fd7a0ed8c6d2283d8f2de4d
PeturIvanov <116626395+PeturIvanov@users.noreply.github.com>
2023-07-05 20:29:03 +0300

    Create photo_album.py
    Solve problem 1

---
+++ python_advanced/OOP/Static and Class Methods - Exercise/photo_album.py
@@ -0,0 +1,41 @@
+import math
+
+
+class PhotoAlbum:
+    MAX_PHOTOS_PER_PAGE = 4
+    def __init__(self, pages: int):
+        self.pages = pages
+        self.photos = [[] for _ in range(self.pages)] # Matrix
+
+    @classmethod
+    def from_photos_count(cls, photos_count: int):
+        pages = math.ceil(photos_count / 4)
+        return cls(pages)
+
+    def add_photo(self, label: str) -> str:
+        for i in range(len(self.photos)):
+            if len(self.photos[i]) < PhotoAlbum.MAX_PHOTOS_PER_PAGE:
+                self.photos[i].append(label)
+
+                return f"{label} photo added successfully on page {i + 1} slot {len(self.photos[i])}"
+
+        else:
+            return "No more free slots"
+
+    def display(self) -> str:
+        result = "-" * 11
+        result += "\n"
+
+        for page_index in range(len(self.photos)):
+
+            if self.photos[page_index]:
+                result += (' '.join(["[]" for _ in range(len(self.photos[page_index]))]))
+                result += "\n"
+
+            else:
+                result += "\n"
+
+            result += "-" * 11
+            result += "\n"
+
+        return result
