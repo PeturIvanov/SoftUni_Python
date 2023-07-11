2aa37c5233b33118b1fa89f75d2d839e4bda2326
PeturIvanov <116626395+PeturIvanov@users.noreply.github.com>
2023-07-11 15:36:27 +0300

    04. Wild Farm

---
+++ python_advanced/OOP/Polymorphism and Abstraction - Exercise/Wild_Animals/project/animals/birds.py
@@ -0,0 +1,29 @@
+from project.animals.animal import Bird
+from project.food import Meat, Fruit, Vegetable, Seed
+
+
+class Owl(Bird):
+    @property
+    def food_that_eats(self) -> list:
+        return [Meat]
+
+    @property
+    def gained_weight(self) -> float:
+        return 0.25
+
+    def make_sound(self) -> str:
+        return "Hoot Hoot"
+
+class Hen(Bird):
+    @property
+    def food_that_eats(self) -> list:
+        return [Vegetable, Meat, Fruit, Seed]
+
+    @property
+    def gained_weight(self) -> float:
+        return 0.35
+
+    def make_sound(self) -> str:
+        return "Cluck"
+
+
