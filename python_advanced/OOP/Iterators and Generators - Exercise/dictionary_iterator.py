f3828cc10f90e55d759a31d557e4999b693875d3
PeturIvanov <116626395+PeturIvanov@users.noreply.github.com>
2023-07-14 23:23:13 +0300

    Update dictionary_iterator.py

--- python_advanced/OOP/Iterators and Generators - Exercise/dictionary_iterator.py
+++ python_advanced/OOP/Iterators and Generators - Exercise/dictionary_iterator.py
@@ -2,9 +2,7 @@ from typing import Dict
 
 class dictionary_iter:
     def __init__(self, dictionary: Dict):
-        self.dictionary = dictionary
-        self.keys = list(self.dictionary.keys())
-        self.values = list(self.dictionary.values())
+        self.items = list(dictionary.items())
         self.index = -1
 
     def __iter__(self):
@@ -12,12 +10,11 @@ class dictionary_iter:
 
     def __next__(self):
         self.index += 1
-        if self.index == len(self.keys):
-            self.index = -1
+
+        if self.index == len(self.items):
             raise StopIteration
 
-        return self.keys[self.index], self.values[self.index]
-
+        return self.items[self.index]
 
 result = dictionary_iter({1: "1", 2: "2"})
 for x in result:
