27ce15b8e63ef3d3d9794676e39e3e0e8fe1f83b
PeturIvanov <116626395+PeturIvanov@users.noreply.github.com>
2023-07-17 10:09:05 +0300

    Update fibonacci_generator.py
    solution

--- python_advanced/OOP/Iterators and Generators - Exercise/fibonacci_generator.py
+++ python_advanced/OOP/Iterators and Generators - Exercise/fibonacci_generator.py
@@ -1,1 +1,13 @@
-n = int(input())
+def fibonacci():
+    fib1 = 0
+    fib2 = 1
+
+    while True:
+        yield fib1
+        fib1 = fib1 + fib2
+        fib1 , fib2 = fib2, fib1
+
+generator = fibonacci()
+for i in range(5):
+    print(next(generator))
+
