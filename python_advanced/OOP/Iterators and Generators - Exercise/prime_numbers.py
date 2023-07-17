ae3be652fc49a5c730756dd159af346091c4b878
PeturIvanov <116626395+PeturIvanov@users.noreply.github.com>
2023-07-17 10:44:05 +0300

    Create prime_numbers.py

---
+++ python_advanced/OOP/Iterators and Generators - Exercise/prime_numbers.py
@@ -0,0 +1,17 @@
+from typing import List
+
+
+def get_primes(numbers: List[int]):
+    for n in numbers:
+        if n <= 1:
+            continue
+
+        for divider in range(2, n + 1):
+            if divider != n and n % divider == 0:
+                break
+
+        else:
+            yield n
+
+
+print(list(get_primes([2, 4, 3, 5, 6, 9, 1, 0])))
