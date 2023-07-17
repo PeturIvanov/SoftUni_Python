5a87d116253626c8b930aeb5594871d69e4f2695
PeturIvanov <116626395+PeturIvanov@users.noreply.github.com>
2023-07-17 10:17:04 +0300

    Create reader.py

---
+++ python_advanced/OOP/Iterators and Generators - Exercise/reader.py
@@ -0,0 +1,10 @@
+def read_next(*args):
+    for seq in args:
+        for el in list(seq):
+            yield el
+
+
+for item in read_next("string", (2,), {"d": 1, "i": 2, "c": 3, "t": 4}):
+    print(item, end='')
+
+
