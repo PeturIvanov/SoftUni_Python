3c265c91481d6f41f81abcd09e0355d4224525bc
PeturIvanov <116626395+PeturIvanov@users.noreply.github.com>
2023-07-19 17:30:45 +0300

    Create HTML_tags.py

---
+++ python_advanced/OOP/Decorators - Exercise/HTML_tags.py
@@ -0,0 +1,17 @@
+def tags(tag):
+    def decorator(func):
+        pass
+
+        def wrapper(*args, **kwargs):
+            return f"<{tag}>{func(*args, **kwargs)}</{tag}>"
+
+        return wrapper
+
+    return decorator
+
+
+@tags('h1')
+def to_upper(text):
+    return text.upper()
+print(to_upper('hello'))
+
