82334cf6d8a4b062322cfff54a276d238c064d45
PeturIvanov <116626395+PeturIvanov@users.noreply.github.com>
2023-07-19 16:19:10 +0300

    Update logged.py

--- python_advanced/OOP/Decorators - Exercise/logged.py
+++ python_advanced/OOP/Decorators - Exercise/logged.py
@@ -1,7 +1,6 @@
 def logged(function):
     def wrapper(*args):
-        result = function(*args)
-        return f"you called {function.__name__}{args}\nit returned {result}"
+        return f"you called {function.__name__}{args}\nit returned {function(*args)}"
 
     return wrapper
 
