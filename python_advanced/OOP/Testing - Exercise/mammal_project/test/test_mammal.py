96a5293ed1ff99c5b16c335f86fcc90e2e3b123d
PeturIvanov <116626395+PeturIvanov@users.noreply.github.com>
2023-07-22 10:39:15 +0300

    tests

---
+++ python_advanced/OOP/Testing - Exercise/mammal_project/test/test_mammal.py
@@ -0,0 +1,32 @@
+from project.mammal import Mammal
+import unittest
+
+
+class MammalTests(unittest.TestCase):
+    def setUp(self) -> None:
+        self.mammal = Mammal("Test", "Test type", "Test sound")
+
+    def test_init_is_correct(self):
+        self.assertEqual("Test", self.mammal.name)
+        self.assertEqual("Test type", self.mammal.type)
+        self.assertEqual("Test sound", self.mammal.sound)
+        self.assertEqual("animals", self.mammal._Mammal__kingdom)
+
+    def test_make_sound_return_correct_data(self):
+        result = self.mammal.make_sound()
+        self.assertEqual("Test makes Test sound", result)
+
+    def test_get_kingdom_getter(self):
+        result = self.mammal.get_kingdom()
+        self.assertEqual("animals", result)
+
+    def test_info_return_correct_data(self):
+        result = self.mammal.info()
+        self.assertEqual("Test is of type Test type", result)
+
+
+if __name__ == "__main__":
+    unittest.main()
+
+
+
