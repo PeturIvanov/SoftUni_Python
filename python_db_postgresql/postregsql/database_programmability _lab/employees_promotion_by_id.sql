c47dbc0acb7cfd72c1e98015a9bb4b085a5fc513
Petur <peturivanov4712@gmail.com>
2023-10-14 13:26:45 +0300

    db_programmability solutions

---
+++ python_db_postgresql/postregsql/database_programmability _lab/employees_promotion_by_id.sql
@@ -0,0 +1,15 @@
+CREATE OR REPLACE PROCEDURE sp_increase_salary_by_id(id INT)
+AS
+$$
+    BEGIN
+        IF (SELECT salary FROM employees WHERE employee_id = id) IS NULL THEN
+            RETURN;
+        ELSE
+            UPDATE employees
+            SET salary = salary + salary * 0.05
+            WHERE employee_id = id;
+        END IF;
+    END;
+$$
+LANGUAGE plpgsql
+;
