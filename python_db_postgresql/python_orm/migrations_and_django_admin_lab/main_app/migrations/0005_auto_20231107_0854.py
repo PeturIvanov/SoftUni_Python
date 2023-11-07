a0def283e1c4b4e12825de74ed074de0c302e591
Petur <peturivanov4712@gmail.com>
2023-11-07 09:07:14 +0200

    model product

---
+++ python_db_postgresql/python_orm/migrations_and_django_admin_lab/main_app/migrations/0005_auto_20231107_0854.py
@@ -0,0 +1,42 @@
+import random
+from django.db import migrations
+
+
+def add_barcode(apps, schema_editor):
+    Product = apps.get_model("main_app", "Product")
+    all_products = Product.objects.all()
+
+    all_barcodes = random.sample(
+        range(
+            100_000_000, 1_000_000_000
+        ),
+        len(all_products)
+    )
+
+    for i in range(len(all_products)):
+        product = all_products[i]
+        product.barcode = all_barcodes[i]
+        product.save()
+
+
+def reverse_add_barcode(apps, schema_editor):
+    Product = apps.get_model("main_app", "Product")
+    all_products = Product.objects.all()
+
+    for product in all_products:
+        product.barcode = 0
+        product.save()
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('main_app', '0004_product_barcode'),
+    ]
+
+    operations = [
+        migrations.RunPython(
+            add_barcode,
+            reverse_code=reverse_add_barcode
+        )
+    ]
