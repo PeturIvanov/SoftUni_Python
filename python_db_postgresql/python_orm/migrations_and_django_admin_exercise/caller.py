0e3626fa7ab6bcbbed8ffdd781c559298c878bd5
Petur <peturivanov4712@gmail.com>
2023-11-07 11:40:58 +0200

    solutions from the judge only

--- python_db_postgresql/python_orm/migrations_and_django_admin_exercise/caller.py
+++ python_db_postgresql/python_orm/migrations_and_django_admin_exercise/caller.py
@@ -5,6 +5,63 @@ import django
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
 django.setup()
 
-# Import your models here
+from main_app.models import Shoe
 
-# Create queries within functions
+
+def add_initial_products():
+    shoe1 = Shoe(
+        brand="Nike",
+        size=34
+    )
+
+    shoe1.save()
+
+    shoe2 = Shoe(
+        brand="Nike",
+        size=31
+    )
+
+    shoe2.save()
+
+    shoe3 = Shoe(
+        brand="Adidas",
+        size=45
+    )
+
+    shoe3.save()
+
+    shoe4 = Shoe(
+        brand="Adidas",
+        size=42
+    )
+
+    shoe4.save()
+
+    shoe5 = Shoe(
+        brand="Puma",
+        size=21
+    )
+
+    shoe5.save()
+
+    return f"{shoe1}, {shoe2}, {shoe3}, {shoe4}, {shoe5}"
+
+
+# print(add_initial_products())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
