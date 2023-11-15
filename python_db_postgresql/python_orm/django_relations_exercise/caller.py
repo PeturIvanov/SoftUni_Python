f404aba58eecc33f6da07353008a591f61f0cfa8
Petur <peturivanov4712@gmail.com>
2023-11-15 19:14:28 +0200

    solutions

--- python_db_postgresql/python_orm/django_relations_exercise/caller.py
+++ python_db_postgresql/python_orm/django_relations_exercise/caller.py
@@ -1,10 +1,123 @@
 import os
+from datetime import timedelta, date
+
 import django
+from django.db.models import Sum, Count
 
 # Set up Django
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
 django.setup()
 
-# Import your models here
+from main_app.models import Author, Book, Artist, Song, Product, Review, DrivingLicense, Driver, Owner, Car, \
+    Registration
 
-# Create queries within functions
+
+def show_all_authors_with_their_books():
+    authors = Author.objects.all().order_by('id')
+
+    result = []
+
+    for author in authors:
+        books_by_author = author.book_set.all()
+
+        if not books_by_author:
+            continue
+
+        result.append(
+            f"{author} has written - {', '.join([str(book) for book in books_by_author])}!"
+        )
+
+    return "\n".join(result)
+
+
+def delete_all_authors_without_books():
+    Author.objects.filter(book__isnull=True).delete()
+
+
+def add_song_to_artist(artist_name: str, song_title: str):
+    artist = Artist.objects.get(name=artist_name)
+    song = Song.objects.get(title=song_title)
+
+    artist.songs.add(song)
+
+
+def get_songs_by_artist(artist_name: str):
+    return Artist.objects.get(name=artist_name).songs.all().order_by('-id')
+
+
+def remove_song_from_artist(artist_name: str, song_title: str):
+    Artist.objects.get(name=artist_name).songs.get(title=song_title).delete()
+
+
+def calculate_average_rating_for_product_by_name(product_name: str):
+    product_ratings = Product.objects.annotate(total_rating=Sum('reviews__rating'),
+                                               num_reviews=Count('reviews')
+                                               ).get(name=product_name)
+
+    avg_rating = product_ratings.total_rating / product_ratings.num_reviews
+
+    return avg_rating
+
+
+def get_reviews_with_high_ratings(threshold: int):
+    return Review.objects.filter(rating__gte=threshold)
+
+
+def get_products_with_no_reviews():
+    return Product.objects.filter(reviews__isnull=True).order_by('-name')
+
+
+def delete_products_without_reviews():
+    Product.objects.filter(reviews__isnull=True).delete()
+
+
+def calculate_licenses_expiration_dates():
+    driving_licenses = DrivingLicense.objects.all().order_by('-license_number')
+
+    result = []
+
+    for dl in driving_licenses:
+        expiration_date = dl.issue_date + timedelta(days=365)
+
+        result.append(
+            f'License with id: {dl.license_number} expires on {expiration_date}!'
+        )
+
+    return '\n'.join(result)
+
+
+def get_drivers_with_expired_licenses(due_date):
+    expiration_date = due_date - timedelta(days=365)
+
+    drivers_with_expired_license = Driver.objects.filter(drivinglicense__issue_date__gt=expiration_date)
+
+    return drivers_with_expired_license
+
+
+def register_car_by_owner(owner: Owner):
+    registration = Registration.objects.filter(car__isnull=True).first()
+    car = Car.objects.filter(registration__isnull=True).first()
+
+    car.owner = owner
+    car.registration = registration
+
+    car.save()
+
+    registration.registration_date = date.today()
+    registration.car = car
+
+    registration.save()
+
+    return f'Successfully registered {car.model}' \
+           f' to {owner.name} with registration number' \
+           f' {registration.registration_number}.'
+
+
+
+
+
+
+
+
+
+
--- python_db_postgresql/python_orm/django_relations_exercise/main_app/models.py
+++ python_db_postgresql/python_orm/django_relations_exercise/main_app/models.py
@@ -1,3 +1,174 @@
 from django.db import models
 
-# Create your models here.
+
+class Author(models.Model):
+    name = models.CharField(
+        max_length=40,
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class Book(models.Model):
+    title = models.CharField(
+        max_length=40,
+    )
+
+    price = models.DecimalField(
+        max_digits=5,
+        decimal_places=2,
+    )
+
+    author = models.ForeignKey(
+        to='Author',
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self):
+        return self.title
+
+
+class Song(models.Model):
+    title = models.CharField(
+        max_length=100,
+        unique=True,
+    )
+
+    def __str__(self):
+        return self.title
+
+
+class Artist(models.Model):
+    name = models.CharField(
+        max_length=100,
+        unique=True,
+    )
+
+    songs = models.ManyToManyField(
+        to='Song',
+        related_name='artists'
+    )
+
+
+class Product(models.Model):
+    name = models.CharField(
+        max_length=100,
+        unique=True,
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class Review(models.Model):
+    description = models.TextField(
+        max_length=200,
+    )
+
+    rating = models.PositiveIntegerField()
+
+    product = models.ForeignKey(
+        to='Product',
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        related_name='reviews'
+    )
+
+
+class Driver(models.Model):
+    first_name = models.CharField(
+        max_length=50,
+    )
+
+    last_name = models.CharField(
+        max_length=50,
+    )
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+
+
+class DrivingLicense(models.Model):
+    license_number = models.CharField(
+        max_length=10,
+        unique=True,
+    )
+
+    issue_date = models.DateField()
+
+    driver = models.OneToOneField(
+        to="Driver",
+        on_delete=models.CASCADE,
+    )
+
+
+class Owner(models.Model):
+    name = models.CharField(
+        max_length=50,
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class Car(models.Model):
+    model = models.CharField(
+        max_length=50,
+    )
+
+    year = models.PositiveIntegerField()
+
+    owner = models.ForeignKey(
+        to='Owner',
+        on_delete=models.CASCADE,
+        related_name='cars',
+        blank=True,
+        null=True,
+    )
+
+    def __str__(self):
+        return self.model
+
+
+class Registration(models.Model):
+    registration_number = models.CharField(
+        max_length=10,
+        unique=True,
+    )
+
+    registration_date = models.DateField(
+        blank=True,
+        null=True,
+    )
+
+    car = models.OneToOneField(
+        to='Car',
+        on_delete=models.CASCADE,
+        blank=True,
+        null=True,
+    )
+
+    def __str__(self):
+        return self.registration_number
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
