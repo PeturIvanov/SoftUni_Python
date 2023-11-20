199f6af2b824893b48a8dc7ba4ad48853bb20b35
Petur <peturivanov4712@gmail.com>
2023-11-20 17:29:11 +0200

    solutions

--- python_db_postgresql/python_orm/models_inheritance_exercise/main_app/models.py
+++ python_db_postgresql/python_orm/models_inheritance_exercise/main_app/models.py
@@ -1,6 +1,15 @@
 from django.db import models
 
 
+class StudentIDField(models.PositiveIntegerField):
+    def to_python(self, value):
+        try:
+            return int(value)
+
+        except ValueError:
+            pass
+
+
 class BaseCharacter(models.Model):
     name = models.CharField(
         max_length=100,
@@ -13,7 +22,6 @@ class BaseCharacter(models.Model):
 
 
 class Mage(BaseCharacter):
-
     elemental_power = models.CharField(
         max_length=100,
     )
@@ -89,6 +97,87 @@ class FelbladeDemonHunter(DemonHunter):
     felblade_ability = models.CharField(
         max_length=100,
     )
+
+
+class UserProfile(models.Model):
+    username = models.CharField(
+        max_length=70,
+        unique=True,
+    )
+
+    email = models.EmailField(
+        unique=True,
+    )
+
+    bio = models.TextField(
+        blank=True,
+        null=True,
+    )
+
+    def __str__(self):
+        return self. username
+
+
+class Message(models.Model):
+    sender = models.ForeignKey(
+        to='UserProfile',
+        on_delete=models.CASCADE,
+        related_name='sent_messages'
+    )
+
+    receiver = models.ForeignKey(
+        to='UserProfile',
+        on_delete=models.CASCADE,
+        related_name='received_messages',
+    )
+
+    content = models.TextField()
+
+    timestamp = models.DateTimeField(
+        auto_now_add=True,
+    )
+
+    is_read = models.BooleanField(
+        default=False,
+    )
+
+    def mark_as_read(self):
+        self.is_read = True
+
+    def mark_as_unread(self):
+        self.is_read = False
+
+    def reply_to_message(self, reply_content: str, receiver: UserProfile):
+        reply_message = Message.objects.create(content=reply_content, receiver=receiver, sender=self.receiver)
+
+        return reply_message
+
+    def forward_message(self, sender: UserProfile, receiver: UserProfile):
+        message = Message.objects.create(sender=sender, receiver=receiver, content=self.content)
+
+        return message
+
+
+class Student(models.Model):
+    name = models.CharField(
+        max_length=100,
+    )
+
+    student_id = StudentIDField()
+
+    def __str__(self):
+        return self.name
+
+
+
+
+
+
+
+
+
+
+
 
 
 
