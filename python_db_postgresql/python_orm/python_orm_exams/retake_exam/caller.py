b70a3ae48d37f8d5ede0ea962055a68df99fbbb2
Petur <peturivanov4712@gmail.com>
2023-12-28 17:53:50 +0200

    retake exam

--- python_db_postgresql/python_orm/python_orm_exams/retake_exam/caller.py
+++ python_db_postgresql/python_orm/python_orm_exams/retake_exam/caller.py
@@ -1,9 +1,114 @@
 import os
 import django
+from django.db.models import Count
 
-# Set up Django
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
 django.setup()
 
-# Import your models here
-# Create and run your queries within functions
+from main_app.models import TennisPlayer, Match, Tournament
+
+
+def get_tennis_players(search_name=None, search_country=None):
+    if search_name is None and search_country is None:
+        return ""
+
+    tennis_players = TennisPlayer.objects.all()
+
+    if search_name:
+        tennis_players = tennis_players.filter(full_name__icontains=search_name)
+
+    if search_country:
+        tennis_players = tennis_players.filter(country__icontains=search_country)
+
+    tennis_players = tennis_players.order_by('ranking')
+
+    result = [
+        f"Tennis Player: {tp.full_name}, country: {tp.country}, ranking: {tp.ranking}"
+        for tp in tennis_players
+    ]
+
+    return "\n".join(result)
+
+
+def get_top_tennis_player():
+    best_player = TennisPlayer.objects.get_tennis_players_by_wins_count().first()
+
+    if not best_player:
+        return ""
+
+    return f"Top Tennis Player: {best_player.full_name} with {best_player.count_wins} wins."
+
+
+def get_tennis_player_by_matches_count():
+    player_with_most_matches = TennisPlayer.objects.annotate(num_matches=Count('matches')
+                                                             ).order_by('-num_matches', 'ranking').first()
+
+    if not player_with_most_matches or player_with_most_matches.num_matches == 0:
+        return ""
+
+    return f"Tennis Player: {player_with_most_matches.full_name}" \
+           f" with {player_with_most_matches.num_matches} matches played."
+
+
+def get_tournaments_by_surface_type(surface=None):
+    if surface is None:
+        return ""
+
+    tournaments = Tournament.objects.filter(surface_type__icontains=surface
+                                            ).annotate(num_matches=Count('match')
+                                                       ).order_by('-start_date')
+
+    if not tournaments:
+        return ""
+
+    result = [
+        f"Tournament: {t.name}, start date: {t.start_date}, matches: {t.num_matches}"
+        for t in tournaments
+    ]
+
+    return "\n".join(result)
+
+
+def get_latest_match_info():
+    last_match = Match.objects.prefetch_related('players__match_set__tournament').order_by('date_played').last()
+
+    if not last_match:
+        return ""
+
+    player1, player2 = last_match.players.all().order_by('full_name')
+
+    winner = last_match.winner.full_name if last_match.winner else "TBA"
+
+    return f"Latest match played on: {last_match.date_played}," \
+           f" tournament: {last_match.tournament.name}, " \
+           f"score: {last_match.score}," \
+           f" players: {player1} vs {player2}," \
+           f" winner: {winner}," \
+           f" summary: {last_match.summary}"
+
+
+def get_matches_by_tournament(tournament_name=None):
+    if tournament_name is None:
+        return "No matches found."
+
+    tournament = Tournament.objects.filter(name__exact=tournament_name).prefetch_related('match_set').first()
+
+    if not tournament:
+        return "No matches found."
+
+    matches = tournament.match_set.all().order_by('-date_played')
+
+    if not matches:
+        return "No matches found."
+
+    result = []
+
+    for match in matches:
+        winner = match.winner.full_name if match.winner else "TBA"
+
+        result.append(f"Match played on: {match.date_played}, "
+                      f"score: {match.score}, "
+                      f"winner: {winner}")
+
+    return "\n".join(result)
+
--- python_db_postgresql/python_orm/python_orm_exams/retake_exam/main_app/models.py
+++ python_db_postgresql/python_orm/python_orm_exams/retake_exam/main_app/models.py
@@ -1,3 +1,102 @@
 from django.db import models
+from django.core.validators import MinLengthValidator, MaxValueValidator, MinValueValidator
 
-# Create your models here.
+from main_app.managers import TennisPlayerManager
+
+
+class TennisPlayer(models.Model):
+    full_name = models.CharField(
+        max_length=120,
+        validators=[MinLengthValidator(5)],
+    )
+
+    birth_date = models.DateField()
+
+    country = models.CharField(
+        max_length=100,
+        validators=[MinLengthValidator(2)],
+    )
+
+    ranking = models.PositiveIntegerField(
+        validators=[
+            MinValueValidator(1),
+            MaxValueValidator(100)
+        ],
+    )
+
+    is_active = models.BooleanField(
+        default=True,
+    )
+
+    objects = TennisPlayerManager()
+
+    def __str__(self):
+        return self.full_name
+
+
+class Tournament(models.Model):
+    class SURFACES(models.TextChoices):
+        not_selected = 'Not Selected',
+        clay = 'Clay',
+        grass = 'Grass',
+        hard_court = 'Hard Court'
+
+    name = models.CharField(
+        max_length=150,
+        validators=[MinLengthValidator(2)],
+        unique=True
+    )
+
+    location = models.CharField(
+        max_length=100,
+        validators=[MinLengthValidator(2)]
+    )
+
+    prize_money = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+    )
+
+    start_date = models.DateField()
+
+    surface_type = models.CharField(
+        max_length=12,
+        choices=SURFACES.choices,
+        default='Not Selected'
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class Match(models.Model):
+    score = models.CharField(
+        max_length=100,
+    )
+
+    summary = models.TextField(
+        validators=[MinLengthValidator(5)],
+    )
+
+    date_played = models.DateTimeField()
+
+    tournament = models.ForeignKey(
+        to='Tournament',
+        on_delete=models.CASCADE,
+    )
+
+    players = models.ManyToManyField(
+        to='TennisPlayer',
+        related_name='matches',
+    )
+
+    winner = models.ForeignKey(
+        to='TennisPlayer',
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True
+    )
+
+    class Meta:
+        verbose_name = 'Matches'
+        verbose_name_plural = 'Matches'
